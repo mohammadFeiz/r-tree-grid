e5b7ac7721249d1a4603f46b4277522588ea780e
MFeiz <30654631+mohammadFeiz@users.noreply.github.com>
2019-10-08 14:00:20 -0800

    .

--- src/columns.js
+++
@@ -1,8 +0,0 @@
-var columns = [
-  {width:50,title:'Code',field:'code'},
-  {width:120,title:'Title',field:'title'},
-  {width:100,title:'Status',field:'status'},
-  {width:140,title:'Organ',field:'organ.name'},
-  {width:100,title:'Owner Role',field:'ownerRole.type'},
-]
-export default columns;
